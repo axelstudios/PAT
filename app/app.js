dc69d89af869181f97cf32d41d28332818ab8c4e
Alex Swindler <Alex.Swindler@nrel.gov>
2015-10-19 14:44:58 -0600

    Moving node modules to the top level

--- app/app.js
+++ app/app.js
@@ -9,7 +9,7 @@ import { greet } from './hello_world/hello_world';
 // are required the same way as always.
 var os = require('os');
 var app = require('remote').require('app');
-var jetpack = require('fs-jetpack').cwd(app.getAppPath());
+var jetpack = require('../node_modules/fs-jetpack').cwd(app.getAppPath());
 
 // Holy crap! This is browser window with HTML and stuff, but I can read
 // here files like it is node.js! Welcome to Electron world :)
@@ -18,7 +18,7 @@ console.log(jetpack.read('package.json', 'json'));
 // window.env contains data from config/env_XXX.json file.
 var envName = window.env.name;
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('greet').innerHTML = greet();
     document.getElementById('platform-info').innerHTML = os.platform();
     document.getElementById('env-name').innerHTML = envName;
